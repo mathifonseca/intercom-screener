dae60d29b0220d02b2a9e4e4fa58d6ee4c3a1e51
Mathi Fonseca <mathifonseca@gmail.com>
2016-07-07 11:06:54 -0300

    finished flattener implementation

--- flattener/src/main/groovy/com/mathifonseca/intercom/list/ListUtils.groovy
+++ flattener/src/main/groovy/com/mathifonseca/intercom/list/ListUtils.groovy
@@ -3,7 +3,21 @@ package com.mathifonseca.intercom.list
 class ListUtils {
 
     static Collection flatten(Collection list) {
+        if (list == null) {
+            throw new IllegalArgumentException()
+        }
+        return flattenRecursive(list, [])
+    }
 
+    private static Collection flattenRecursive(Collection list, Collection flattened) {
+        list.each { item ->
+            if (item instanceof List) {
+                flattenRecursive(item, flattened)
+            } else {
+                flattened << item
+            }
+        }
+        return flattened
     }
 
 }
