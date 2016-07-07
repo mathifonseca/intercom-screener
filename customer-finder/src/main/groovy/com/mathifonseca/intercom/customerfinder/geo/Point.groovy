f3f89de881f99d52fc01d734bdc35461cfb48334
Mathi Fonseca <mathifonseca@gmail.com>
2016-07-07 15:07:50 -0300

    implemented geographic operations

--- customer-finder/src/main/groovy/com/mathifonseca/intercom/customerfinder/geo/Point.groovy
+++ customer-finder/src/main/groovy/com/mathifonseca/intercom/customerfinder/geo/Point.groovy
@@ -5,7 +5,38 @@ class Point {
     double latitude
     double longitude
 
+    void setLatitude(double lat) {
+        if (lat > 90 || lat < -90) {
+            throw new IllegalArgumentException()
+        }
+        this.latitude = lat
+    }
+
+    void setLongitude(double lng) {
+        if (lng > 180 || lng < -180) {
+            throw new IllegalArgumentException()
+        }
+        this.longitude = lng
+    }
+
     boolean isWithinKms(double kms, Point other) {
+        if (kms < 0) {
+            throw new IllegalArgumentException()
+        }
+        return GeoUtils.distance(this, other) <= kms
+    }
+
+    static Point fromString(String coordinates) {
+        if (!(coordinates ==~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/)) {
+            throw new IllegalFormatException()
+        }
+        def parts = coordinates.split(',')
+        return new Point(latitude: parts[0].toDouble(), longitude: parts[1].toDouble())
+    }
+
+    @Override
+    String toString() {
+        return "$latitude,$longitude"
     }
 
 }
